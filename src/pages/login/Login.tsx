025ffbfc7488816a586904360af54e5eab638585
cat66123 <326700510@qq.com>
2019-09-03 17:08:03 +0800

    login 页面样式

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,11 +1,32 @@
 import * as React from "react";
-import { Button } from "antd";
+import { Input, Button } from "antd";
 
-import { LoginWrapper } from "./style";
+import { LoginWrapper, ContentWrapper } from "./style";
 
 class Login extends React.Component {
   render() {
-    return <LoginWrapper></LoginWrapper>;
+    return (
+      <LoginWrapper>
+        <ContentWrapper>
+          <div className="login-logo">
+            <span>Skill</span>
+          </div>
+          <div className="login-top">
+            <div className="login-input">
+              <Input size="large" placeholder="请输入账号" />
+            </div>
+            <div className="login-input">
+              <Input.Password size="large" placeholder="请输入密码" />
+            </div>
+          </div>
+          <div className="login-bottom">
+            <Button size="large" type="primary" block>
+              登录
+            </Button>
+          </div>
+        </ContentWrapper>
+      </LoginWrapper>
+    );
   }
 }
 
--- src/pages/login/style.tsx
+++ src/pages/login/style.tsx
@@ -1,7 +1,52 @@
 import styled from "styled-components";
 
 export const LoginWrapper = styled.div`
-  width: 100px;
-  height: 100px;
-  background: green;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  background: linear-gradient(to bottom, #cfdeee 0%, #8da9cf 100%);
+`;
+
+export const ContentWrapper = styled.div`
+  width: 400px;
+  height: 300px;
+  background: #6699cc;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+
+  .login-logo {
+    flex-grow: 2;
+    display: flex;
+
+    span {
+      margin: auto;
+      font-size: 40px;
+      color: white;
+    }
+  }
+
+  .login-top {
+    flex-grow: 2;
+    display: flex;
+    flex-direction: column;
+    padding: 10px 30px;
+
+    .login-input {
+      flex: 1;
+    }
+  }
+
+  .login-bottom {
+    flex-grow: 2;
+    padding: 0px 30px;
+
+    button {
+      background: #336699;
+      border: none;
+    }
+    button:hover {
+      background: rgba(33, 66, 99, 0.8);
+    }
+  }
 `;
