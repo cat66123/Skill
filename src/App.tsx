b28afe23d750c68fca590a4321275281506cea43
cat66123 <326700510@qq.com>
2019-09-03 11:47:35 +0800

    react基本构成

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+class App extends React.Component {
+  render() {
+    return <h1>Cat</h1>;
+  }
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import PageInterface from "./PageInterface";
+import App from "./App";
 
-class IndexComponent extends React.Component<PageInterface, {}> {
-  render() {
-    return <h1>hello world!!!123{this.props.color}</h1>;
-  }
-}
-
-ReactDOM.render(
-  <IndexComponent color="Blue" />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
